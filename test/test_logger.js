07958408eb3ded9dc009b4ad9ffdf006f54a4faa
Ian Laird <irlaird@gmail.com>
2017-06-05 14:42:03 -0600

    Added unit tests for new logger context.

--- test/test_logger.js
+++ test/test_logger.js
@@ -32,4 +32,33 @@ describe('Logger', () => {
         var x = new rpcutil.Logger({ header: "nothing" });
         assert(m in x, "Logger has no " + m + " method.");
     });
+
+    it('Can log for arbitrary contexts.', () => {
+        var x = new rpcutil.Logger({ header: "nothing" });
+        x.context.add('arbitrary', "value");
+        assert(x.context.toString() === '{arbitrary:value}');
+    });
+
+    it('Can log for many arbitrary contexts.', () => {
+        var x = new rpcutil.Logger({ header: "nothing" });
+        x.context.add('arbitrary1', "value1");
+        x.context.add('arbitrary2', "value2");
+        assert(x.context.toString() === '{arbitrary1:value1,arbitrary2:value2}');
+    });
+
+    it('Can remove arbitrary contexts.', () => {
+        var x = new rpcutil.Logger({ header: "nothing" });
+        x.context.add('arbitrary1', "value1");
+        x.context.add('arbitrary2', "value2");
+        x.context.remove('arbitrary2');
+        assert(x.context.toString() === '{arbitrary1:value1}');
+    });
+
+    it('Can reset contexts.', () => {
+        var x = new rpcutil.Logger({ header: "nothing" });
+        x.context.add('arbitrary1', "value1");
+        x.context.add('arbitrary2', "value2");
+        x.context.clear();
+        assert(x.context.toString() === '{}');
+    });
 });
